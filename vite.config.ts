5d8bb4ef9fb4892fe89847d2e9b813764910d768
Joanne Wang <joanneccwang@gmail.com>
2024-06-02 17:03:33 +0800

    feat: resolve src alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import { resolve } from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/PodLandingPage/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@styles': resolve(__dirname, 'src/styles'),
+      '@components': resolve(__dirname, 'src/components'),
+    },
+  },
 });
