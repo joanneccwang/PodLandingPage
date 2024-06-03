d19828ac41225c58aeec8c6e8e74a484a74b58a2
Joanne Wang <joanneccwang@gmail.com>
2024-06-03 16:10:07 +0800

    feat: validate email

--- src/components/EmailRequest.tsx
+++ src/components/EmailRequest.tsx
@@ -1,6 +1,34 @@
+import { useState } from 'react';
 import { mq } from '@utils/breakpoints';
 
 function EmailRequest() {
+  const [email, setEmail] = useState('');
+  const [errorState, setErrorState] = useState<{
+    isError: null | boolean;
+    errorMsg: string;
+  }>({
+    isError: null,
+    errorMsg: '',
+  });
+
+  const handleInputEmail = (e: React.FormEvent) => {
+    const value = (e.target as HTMLInputElement)?.value;
+    setEmail(value);
+  };
+  const handleSubmit = () => {
+    const emailRegex = /[a-zA-Z0-9.]+@[a-zA-Z0-9.]+/g;
+    if (email.length === 0) {
+      setErrorState({ isError: true, errorMsg: 'Oops! Please add your email' });
+    } else if (!emailRegex.test(email)) {
+      setErrorState({
+        isError: true,
+        errorMsg: 'Oops! Please check your email',
+      });
+    } else {
+      // normally do some api calls here
+      setErrorState({ isError: false, errorMsg: 'Successfully requested!' });
+    }
+  };
   return (
     <div
       css={mq({
@@ -28,14 +56,34 @@ function EmailRequest() {
         <input
           type='email'
           placeholder='Email Address'
-          css={mq({
-            position: 'absolute',
-            top: 0,
-            width: 'calc(100% - 64px)',
-            height: '100%',
-            backgroundColor: 'transparent',
-          })}
+          maxLength={1000}
+          css={(theme) =>
+            mq({
+              position: 'absolute',
+              top: 0,
+              width: 'calc(100% - 64px)',
+              height: '100%',
+              fontWeight: 400,
+              backgroundColor: 'transparent',
+              color: theme.colors.white,
+            })
+          }
+          onInput={handleInputEmail}
         ></input>
+
+        {errorState.isError !== null && (
+          <div
+            css={(theme) => ({
+              color: errorState.isError ? theme.colors.red : theme.colors.green,
+              position: 'absolute',
+              top: 'calc(100% + 8px)',
+              fontSize: '12px',
+              fontWeight: 700,
+            })}
+          >
+            {errorState.errorMsg}
+          </div>
+        )}
       </div>
       <button
         css={(theme) =>
@@ -43,6 +91,7 @@ function EmailRequest() {
             backgroundColor: theme.colors.green,
             color: theme.colors.black,
             borderRadius: theme.borderRadius,
+            cursor: 'pointer',
             fontWeight: 400,
             position: ['relative', 'absolute', 'absolute'],
             width: '100%',
@@ -50,8 +99,12 @@ function EmailRequest() {
             height: ['46px', 'calc(100% - 10px)', 'calc(100% - 10px)'],
             top: ['auto', '5px', '5px'],
             right: ['auto', '5px', '5px'],
+            '&:hover': {
+              backgroundColor: theme.colors.greenHover,
+            },
           })
         }
+        onClick={() => handleSubmit()}
       >
         Request Access
       </button>
--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,13 @@ import { ThemeProvider } from '@emotion/react';
 const theme = {
   colors: {
     green: '#54E6AF',
+    greenHover: '#B3FFE2',
     black: '#121725',
     gray3: '#2C344B',
     gray2: '#5A668A',
     gray1: '#C2CBE5',
     white: '#FFFFFF',
+    red: '#FB3E3E',
   },
   borderRadius: '28px',
   fontFamily: 'Chivo, Inter, system-ui, Avenir, Helvetica, Arial, sans-serif',
