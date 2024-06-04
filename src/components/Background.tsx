5f0ea2577aaf459e2da86565257cb22b6868c5fe
Joanne Wang <joanneccwang@gmail.com>
2024-06-04 16:27:33 +0800

    refactor: use picture tag to handle responsive image

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,13 +1,54 @@
 import DotImg from '@assets/desktop/bg-pattern-dots.svg';
 import DesktopBg from '@assets/desktop/image-host.jpg';
 import PadBg from '@assets/tablet/image-host.jpg';
-import mobileBg from '@assets/mobile/image-host.jpg';
+import MobileBg from '@assets/mobile/image-host.jpg';
 
-import useCheckDevice from '@hooks/useCheckDevice';
+import { mq, breakpoints } from '@/utils/breakpoints';
+
+const BackgroundImage = () => {
+  return (
+    <picture>
+      <source media={`(max-width: ${breakpoints[0]}px)`} srcSet={MobileBg} />
+      <source media={`(max-width: ${breakpoints[1]}px)`} srcSet={PadBg} />
+      <img
+        src={DesktopBg}
+        alt='background image'
+        css={mq({
+          position: ['relative', 'absolute', 'absolute'],
+          top: '0px',
+          right: '0px',
+          transform: [
+            '',
+            'translateY(calc(50vh - 50%))',
+            'translateY(calc(50vh - 50%))',
+          ],
+          filter: ['brightness(20%)', 'none', 'none'],
+          objectFit: ['cover', 'none', 'none'],
+          height: ['100%', 'unset', 'unset'],
+        })}
+      />
+    </picture>
+  );
+};
+
+const DottedImage = () => {
+  return (
+    <img
+      src={DotImg}
+      alt=''
+      css={mq({
+        display: ['none', 'inline', 'inline'],
+        position: ['initial', 'absolute', 'absolute'],
+        right: ['initial', 'initial', '0px'],
+        left: ['initial', '0px', 'initial'],
+        bottom: ['initial', '0px', '0px'],
+        transform: ['unset', 'unset', 'translateY(-50%)'],
+      })}
+    />
+  );
+};
 
 function Background() {
-  const { isMobile, isPad } = useCheckDevice();
-  const isPC = !isMobile && !isPad;
   return (
     <div
       css={{
@@ -16,64 +57,8 @@ function Background() {
         position: 'relative',
       }}
     >
-      {isPC && (
-        <div>
-          <img
-            src={DesktopBg}
-            alt='background'
-            css={{
-              position: 'absolute',
-              top: '0px',
-              right: '0px',
-              transform: 'translateY(calc(50vh - 50%))',
-            }}
-          />
-          <img
-            src={DotImg}
-            alt=''
-            css={{
-              position: 'absolute',
-              right: '0px',
-              bottom: '0px',
-              transform: 'translateY(-50%)',
-            }}
-          />
-        </div>
-      )}
-      {isPad && (
-        <div>
-          <img
-            src={PadBg}
-            alt='background'
-            css={{
-              height: '70vh',
-              position: 'absolute',
-              top: '0px',
-              right: '0px',
-            }}
-          />
-          <img
-            src={DotImg}
-            alt=''
-            css={{
-              position: 'absolute',
-              bottom: '0px',
-              left: '0px',
-            }}
-          />
-        </div>
-      )}
-      {isMobile && (
-        <img
-          src={mobileBg}
-          alt='background'
-          css={{
-            filter: 'brightness(20%)',
-            objectFit: 'cover',
-            height: '100%',
-          }}
-        ></img>
-      )}
+      <BackgroundImage />
+      <DottedImage />
     </div>
   );
 }
